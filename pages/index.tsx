3b409dda94a399128b7d710a45d2ceb439356cff
Anyesh <sthacruz@gmail.com>
2021-05-02 09:55:36 +0545

    fix: Remove layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
-import { Layout } from '../components/Layout'
+import { Footer } from '../components/Footer'
+import { Nav } from '../components/Navbar'
 
 export default function index() {
   return (
     <div>
-      <Layout />
+      <Nav />
+      <div className="container mx-auto">
+        <div className="grid lg:grid-cols-6 sm:grid-cols-3 gap-3">
+          <p>asd</p>
+          <p>asd</p>
+          <p>asd</p>
+          <p>asd</p>
+          <p>asd</p>
+          <p>asd</p>
+          <p>asd</p>
+          <p>asd</p>
+          <p>asd</p>
+          <p>asdasdlaksdl;asdka;sld</p>
+          <p>asd</p>
+          <p>asd</p>
+        </div>
+
+        {/* <ImageCard /> */}
+      </div>
+      <Footer />
     </div>
   )
 }
