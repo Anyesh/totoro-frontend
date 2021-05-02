ae1d95570d9c805ffa0aae06513508e378e261a0
Anyesh <sthacruz@gmail.com>
2021-05-02 09:55:03 +0545

    new: Updating footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className="bg-indigo-400 text-white h-screen w-screen">
-      <div className="grid grid-cols-3">
-        <h1>Contact</h1>
-        <h1>Links</h1>
-        <h1>Address</h1>
-      </div>
+    <div className="flex w-screen bg-white flex-row shadow-lg border-red-500 ">
+      <p className="flex-1">Terms of Service</p>
+      <p className="flex-1">Privacy Policy</p>
+      <p className="flex-1">Help</p>
+      <p className="flex-1">Android App</p>
+      <p className="flex-1">Users</p>
+      <p className="flex-1">Collections</p>
+      <p className="flex-1">Explore</p>
     </div>
   )
 }
