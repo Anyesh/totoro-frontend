f6087231441fb203711b353575f96dae6905455c
Anyesh <sthacruz@gmail.com>
2021-05-29 14:33:25 +0545

    fix: Add temp unauth handler

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
+import { IStore } from '@interfaces/general'
 import { useTheme } from 'next-themes'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
 export default function index(): React.ReactElement {
   const { theme } = useTheme()
+  const auth = useSelector((state: IStore) => state.auth)
+  useEffect(() => {
+    if (!auth.isAuthenticated) {
+      window.location.href = '/login'
+    }
+  }, [auth.isAuthenticated])
+
   return (
     <>
       <div className="light:bg-white dark:bg-nord0 h-full">Current model: {theme}</div>
