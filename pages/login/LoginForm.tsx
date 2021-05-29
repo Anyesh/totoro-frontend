aca7646a26911da74f5b4a7c7d04424e1dc871d0
Anyesh <sthacruz@gmail.com>
2021-05-29 14:34:06 +0545

    fix: Update static types and designs

--- pages/login/LoginForm.tsx
+++ pages/login/LoginForm.tsx
@@ -1,19 +1,34 @@
+import { LoginUserData } from '@interfaces/user'
 import React from 'react'
 
-export default function LoginForm(): React.ReactElement {
+export default function LoginForm({
+  state,
+  handleChange,
+  handleSubmission,
+}: {
+  state: LoginUserData
+  handleChange: React.ChangeEventHandler<HTMLInputElement>
+  handleSubmission: React.FormEventHandler<HTMLFormElement>
+}): React.ReactElement {
   return (
     <>
-      <form className="mt-5  mb-5 flex flex-col dark:text-nord0">
+      <form className="mt-5 mb-5 flex flex-col dark:text-nord0" onSubmit={handleSubmission}>
         <input
           type="text"
           placeholder="Username or Email"
+          name="userName"
           required={true}
+          value={state.userName}
+          onChange={handleChange}
           className="mb-3 py-3 px-4 border border-nord9 focus:outline-none rounded-md focus:ring-1 dark:bg-nord4"
         />
         <input
           type="password"
           placeholder="Pasword"
+          name="password"
           required={true}
+          value={state.password}
+          onChange={handleChange}
           className="mb-3 py-3 px-4 border border-nord9 focus:outline-none rounded-md focus:ring-1 ring-cyan-500 dark:bg-nord4"
         />
         <button className="w-full bg-nord10 hover:bg-nord9 transition duration-700  text-white p-3 rounded-lg font-semibold text-lg">
--- pages/signup/Signup.tsx
+++ pages/signup/Signup.tsx
@@ -6,17 +6,24 @@ const pageDescriptions = 'Totoro is an AI-enalbled social networking web applica
 
 export default function Signup(): React.ReactElement {
   return (
-    <div className="lg:pl-20 lg:pr-20 md:pl-10 md:pr-10 sm:p-5 h-screen w-screen grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 items-center justify-center dark:bg-nord1 bg-white">
+    <div className="lg:pl-20 lg:pr-20 md:pl-10 md:pr-10 sm:p-5 h-screen w-screen grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 items-center justify-center">
       <div className="text-3xl text-center sm:text-center mx-auto md:text-left w-full">
         <h1 className="text-5xl lg:text-left light:text-nord1 dark:text-nord4 font-bold">Totoro</h1>
         <p className="dark:text-nord4 light:text-nord0">{pageDescriptions}</p>
       </div>
-      <div className="mx-auto items-center xl:w-1/2 md:w-full sm:w-full  shadow-lg light:bg-white dark:bg-nord3 bg-nord6 rounded-lg p-5">
+      <div className="mx-auto items-center xl:w-1/2 w-full shadow-xl light:bg-white dark:bg-nord3 bg-nord5 rounded-lg p-5">
         <h1 className="font-light  dark:text-white light:text-nord2 text-left p-2"> Signup</h1>
         <hr className="opacity-20" />
 
         <SignupForm />
 
+        <p className="text-center text-sm m-4">
+          By signing up you agree to our{' '}
+          <span className="font-semibold text-center w-full">
+            <Link href="/terms">terms & conditions</Link>
+          </span>
+          .
+        </p>
         <hr className="opacity-30" />
 
         <div className="text-center text-lg mt-4 mb-4 dark:text-white light:text-nord2  p-2 rounded-lgtext-lg ">
