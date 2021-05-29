eb7b4e98a0270b7507713a6e44296c0f6face8a5
Anyesh <sthacruz@gmail.com>
2021-05-29 14:32:48 +0545

    fix: Update render props

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -3,12 +3,13 @@ import { Footer } from '../Footer'
 import { Nav } from '../Navbar'
 
 export default function Layout({ children }: { children: React.ReactNode }): React.ReactElement {
+  const NAME = 'Totoro'
   return (
     <div className="overflow-hidden">
-      {Nav({ name: 'Totoro' })}
+      <Nav name={NAME} />
 
-      <div className="container">{children}</div>
-      {Footer({ name: 'Totoro' })}
+      <div className="container w-screen max-w-full bg-nord6 dark:bg-nord1">{children}</div>
+      <Footer name={NAME} />
     </div>
   )
 }
