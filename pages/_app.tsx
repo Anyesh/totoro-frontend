b585a2053d491f50cfe7fdb6b4a15c8a4a6d9175
Anyesh <sthacruz@gmail.com>
2021-05-30 13:25:23 +0545

    fix: Change to FC for now

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,45 @@
-/* eslint-disable react/react-in-jsx-scope */
 import { Layout } from '@components/Layout'
-import { AppWithStore } from '@interfaces'
-import { makeStore } from '@store'
-import { createWrapper } from 'next-redux-wrapper'
+import axiosInstance from '@config/axios-config'
+import { useStore } from '@store'
 import { ThemeProvider } from 'next-themes'
-import App, { AppContext, AppInitialProps } from 'next/app'
+import { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import React, { useEffect } from 'react'
+import { Provider } from 'react-redux'
 import '../styles/globals.css'
 
-class Totoro extends App<AppWithStore> {
-  static async getInitialProps({ Component, ctx }: AppContext): Promise<AppInitialProps> {
-    const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {}
+const Totoro = ({ Component, pageProps }: AppProps): React.ReactElement => {
+  // async function getInitialProps({ Component, ctx }: AppContext): Promise<AppInitialProps> {
+  //   const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {}
 
-    return { pageProps }
-  }
+  //   return { pageProps }
+  // }
 
-  render() {
-    const { Component, pageProps } = this.props
+  const store = useStore(pageProps.initialReduxState)
 
-    return (
+  const router = useRouter()
+
+  useEffect(() => {
+    axiosInstance('/api/user/whoami/', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => console.log(res.data))
+      .catch(() => {
+        router.push('/login')
+      })
+  }, [])
+
+  return (
+    <Provider store={store}>
       <ThemeProvider attribute="class">
         <Layout>
           <Component {...pageProps} />
         </Layout>
       </ThemeProvider>
-    )
-  }
+    </Provider>
+  )
 }
 
-const wrapper = createWrapper(makeStore)
-export default wrapper.withRedux(Totoro)
+export default Totoro
