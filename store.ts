49c6bd2c2443ef5f71934e2548e214ccab47a926
Anyesh <sthacruz@gmail.com>
2021-05-30 13:17:25 +0545

    new: Add new store

--- store.ts
+++ store.ts
@@ -1,22 +1,49 @@
-import { createWrapper } from 'next-redux-wrapper'
+import { useMemo } from 'react'
 import { applyMiddleware, createStore, Store as ReduxStore } from 'redux'
 import { createLogger } from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import rootReducers from './reducers'
 
-const dev: boolean = process.env.NODE_ENV !== 'production'
-
 const initialState = {}
+
+export type Store = ReduxStore<typeof initialState>
+
+let store: Store | undefined = undefined
+
+const dev: boolean = process.env.NODE_ENV !== 'production'
 
 const { composeWithDevTools } = dev
   ? require('redux-devtools-extension')
   : require('redux-devtools-extension/logOnlyInProduction')
 
-export type Store = ReduxStore<typeof initialState>
-
 const middlewares = dev ? [thunkMiddleware, createLogger()] : []
 
-export const makeStore = (state = initialState): Store => {
+function initStore(state: typeof initialState) {
   return createStore(rootReducers, state, composeWithDevTools(applyMiddleware(...middlewares)))
 }
-export const wrapper = createWrapper(makeStore)
+export const makeStore = (preloadedState: typeof initialState): Store => {
+  let _store = store ?? initStore(preloadedState)
+
+  // After navigating to a page with an initial Redux state, merge that state
+  // with the current state in the store, and create a new store
+  if (preloadedState && store) {
+    _store = initStore({
+      ...store.getState(),
+      ...preloadedState,
+    })
+    // Reset the current store
+    store = undefined
+  }
+
+  // For SSG and SSR always create a new store
+  if (typeof window === 'undefined') return _store
+  // Create the store once in the client
+  if (!store) store = _store
+
+  return _store
+}
+
+export function useStore(state: typeof initialState): Store {
+  const store = useMemo(() => makeStore(initialState), [state])
+  return store
+}
