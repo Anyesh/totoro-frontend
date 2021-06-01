56fe0d42504cc5e6dda821969ffc6803892bae50
Anyesh <sthacruz@gmail.com>
2021-06-01 19:18:24 +0545

    fix: Update nav

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Footer } from '../Footer'
-import { Nav } from '../Navbar'
+import Nav from '../Navbar'
 
 export default function Layout({ children }: { children: React.ReactNode }): React.ReactElement {
   const NAME = 'Totoro'
