13096bac713db3e7d69062c4be938ccce4c8fca1
Anyesh <sthacruz@gmail.com>
2021-06-02 19:27:34 +0545

    new: Add example page

---
+++ pages/u/[username].tsx
@@ -0,0 +1,44 @@
+import { getUserDetails } from '@actions/auth'
+import withAuth from '@hocs/withAuth'
+import { Session } from 'next-auth'
+import { useRouter } from 'next/router'
+import React, { useEffect, useState } from 'react'
+
+interface IPost {
+  details: string
+}
+function User({ session }: { session: Session }): React.ReactElement {
+  const router = useRouter()
+
+  const [posts, setPosts] = useState<IPost>()
+
+  useEffect(() => {
+    if (!session) {
+      return
+    }
+
+    const getDetails = async () => {
+      const d = await getUserDetails()
+      setPosts(d)
+    }
+    getDetails()
+  }, [session])
+
+  return (
+    <div>
+      <h1>User: {router.query.username}</h1>
+      <h1>{posts?.details}</h1>
+      <pre>{JSON.stringify(router, null, 2)}</pre>
+    </div>
+  )
+}
+
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   // Return a list of possible value for id
+// }
+
+// export const getStaticProps: GetStaticProps = async ({ params }) => {
+//   // Return a list of possible value for id
+// }
+
+export default withAuth(User)
