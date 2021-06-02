79b454ea2e74cbca49f4b63895ff6718dc9608a1
Anyesh <sthacruz@gmail.com>
2021-06-02 19:26:27 +0545

    new: Add temp index

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import Loading from '@components/Common/Loading'
 import withAuth from '@hocs/withAuth'
 import { Session } from 'next-auth'
 import { signOut } from 'next-auth/client'
-import Link from 'next/link'
 import React from 'react'
 
 function index(props: { session: Session }) {
@@ -12,10 +11,9 @@ function index(props: { session: Session }) {
     session && (
       <>
         <Loading />
-        Signed in as {session?.user?.email} <br />
+        Signed in as {session?.user?.name} <br />
         <button onClick={() => signOut()}>Sign out</button>
         {session.accessToken && <pre>User has access token</pre>}
-        <Link href="/posts">Go to posts</Link>
       </>
     )
   )
