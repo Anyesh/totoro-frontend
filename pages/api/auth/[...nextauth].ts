ce87dceaefd99116a3f911500b57f7400cda6ca9
Anyesh <sthacruz@gmail.com>
2021-06-02 19:27:09 +0545

    fix: Simplify nextauth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,5 @@
-import { ROOT_API } from '@config'
-import { isJwtExpired, makeUrl } from '@utils/jwt'
-import axios from 'axios'
+import { getInitialTokenFromDRF, refreshTokenFromDRF } from '@actions/auth'
+import { isJwtExpired } from '@utils/jwt'
 import { NextApiRequest, NextApiResponse } from 'next'
 import NextAuth, { NextAuthOptions } from 'next-auth'
 import Providers from 'next-auth/providers'
@@ -9,35 +8,12 @@ import Providers from 'next-auth/providers'
 //   data: ServerData
 // }
 
-interface ServerData {
-  user: {
-    pk: string
-    username: string
-  }
-  access_token: string
-  refresh_token: string
-}
-
-export const refreshToken = async function (refreshToken: string): Promise<string[] | null[]> {
-  try {
-    const response = await axios.post(makeUrl(ROOT_API, 'auth', 'token', 'refresh'), {
-      refresh: refreshToken,
-    })
-
-    const { access, refresh } = response.data
-    // still within this block, return true
-    return [access, refresh]
-  } catch {
-    return [null, null]
-  }
-}
-
 const settings: NextAuthOptions = {
   pages: { signIn: '/login', error: '/error' },
   secret: process.env.SESSION_SECRET,
   session: {
     jwt: true,
-    maxAge: 24 * 60 * 60, // 24 hours
+    maxAge: 6 * 60 * 60, // 6 hours
   },
   jwt: {
     secret: process.env.JWT_SECRET || 'totoro-secret',
@@ -59,58 +35,41 @@ const settings: NextAuthOptions = {
     }),
   ],
   callbacks: {
-    jwt: async (token, user, account, profile, isNewUser) => {
+    jwt: async (token, user, account) => {
       if (user) {
-        console.log('user', user)
-        console.log('account', account)
-        console.log('profile', profile)
-        console.log('newuser', isNewUser)
         if (account?.provider) {
           const { accessToken, idToken } = account
 
-          let local_token = {}
+          const token_response = getInitialTokenFromDRF({
+            provider: account.provider,
+            accessToken,
+            idToken,
+          })
 
-          try {
-            // make a POST request to the DRF backend
-            const tokenPromise = await axios.post<ServerData>(
-              makeUrl(ROOT_API, 'social', account.provider),
-              {
-                access_token: accessToken, // note the differences in key and value variable names
-                id_token: idToken,
-              }
-            )
-
-            // extract the returned token from the DRF backend and add it to the `user` object
-            const { access_token, refresh_token, user } = tokenPromise.data
-            // reform the `token` object from the access token we appended to the `user` object
-            local_token = {
-              ...token,
-              ...user,
-              accessToken: access_token,
-              refreshToken: refresh_token,
-            }
-          } catch (err) {
-            local_token = {}
+          return {
+            ...token,
+            ...(await token_response).user,
+            accessToken: (await token_response).access_token,
+            refreshToken: (await token_response).refresh_token,
           }
-          return local_token
         }
       }
 
       // user was signed in previously, we want to check if the token needs refreshing
       // token has been invalidated, try refreshing it
       if (isJwtExpired(token.accessToken as string)) {
-        const [newAccessToken, newRefreshToken] = await refreshToken(token.refreshToken as string)
+        const [newAccessToken, newRefreshToken] = await refreshTokenFromDRF(
+          token.refreshToken as string
+        )
 
         if (newAccessToken && newRefreshToken) {
-          token = {
+          return {
             ...token,
             accessToken: newAccessToken,
             refreshToken: newRefreshToken,
             iat: Math.floor(Date.now() / 1000),
             exp: Math.floor(Date.now() / 1000 + 2 * 60 * 60),
           }
-
-          return token
         }
 
         // unable to refresh tokens from DRF backend, invalidate the token
