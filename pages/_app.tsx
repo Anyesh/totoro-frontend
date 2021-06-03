e5115480ae3eefd49cea87b1287e486e45ae5355
Anyesh <sthacruz@gmail.com>
2021-06-03 17:16:17 +0545

    fix: Remove useeffect from main app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,11 @@
-import { Layout } from '@components/Layout'
-import setAuthToken from '@config/setAuthToken'
+import Layout from '@components/Layout'
 import { AuthProvider } from '@providers/Auth'
 import { NextPageContext } from 'next'
 import { Session } from 'next-auth'
 import { getSession, Provider } from 'next-auth/client'
 import { ThemeProvider } from 'next-themes'
 import App, { AppContext, AppInitialProps, AppProps } from 'next/app'
-import React, { useEffect } from 'react'
+import React from 'react'
 import { ToastProvider } from 'react-toast-notifications'
 import '../styles/globals.css'
 
@@ -15,17 +14,12 @@ interface AuthAppProps extends AppProps {
 }
 
 const Totoro = ({ Component, pageProps, session }: AuthAppProps): React.ReactElement => {
-  useEffect(() => {
-    setAuthToken(session?.accessToken as string)
-    console.log(session)
-  }, [session])
-
   return (
     <Provider
       session={pageProps.session}
       options={{
         clientMaxAge: 60,
-        keepAlive: 4 * 60,
+        keepAlive: 6 * 60,
       }}
     >
       <AuthProvider authenticated={!!session}>
@@ -45,6 +39,7 @@ Totoro.getInitialProps = async (
   context: unknown
 ): Promise<AppInitialProps & { session: Session | null }> => {
   const session = await getSession(context as NextPageContext)
+  console.log('Initial prop running')
 
   // Call the page's `getInitialProps` and fill `appProps.pageProps`
   const appProps = await App.getInitialProps(context as AppContext)
