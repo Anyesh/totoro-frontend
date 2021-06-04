585531f35f1daf9dac170dcdadcbba1166298c51
Anyesh <sthacruz@gmail.com>
2021-06-04 15:11:18 +0545

    chore: Tidy up the index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,11 @@
-import Loading from '@components/Common/Loading'
 import withAuth from '@hocs/withAuth'
 import { Session } from 'next-auth'
-import { signOut } from 'next-auth/client'
 import React from 'react'
 
 function index(props: { session: Session }) {
   const { session } = props
 
-  return (
-    session && (
-      <>
-        <Loading />
-        Signed in as {session?.user?.name} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-        {session.accessToken && <pre>User has access token</pre>}
-      </>
-    )
-  )
+  return <></>
 }
 
 export default withAuth(index)
