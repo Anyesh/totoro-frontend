b863f92b3f0ac72092f4cfbf632dc87e2f86ad78
Anyesh <sthacruz@gmail.com>
2021-06-04 18:30:25 +0545

    new: Add thirdparty domains

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  images: {
+    domains: ['source.unsplash.com', 'images.unsplash.com', 'api.pexels.com', 'images.pexels.com'],
+  },
   i18n: {
     locales: ['en-US'],
     defaultLocale: 'en-US',
