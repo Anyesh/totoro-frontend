cf3733848a155cb370472cf3157d1d60a531aeb8
Anyesh <sthacruz@gmail.com>
2021-06-05 19:39:29 +0545

    chore: Add padding on loading

--- components/Common/Loading.tsx
+++ components/Common/Loading.tsx
@@ -2,10 +2,10 @@ import Loadin from '@assets/IconComponents/Loadin'
 import React from 'react'
 export default function Loading(): React.ReactElement {
   return (
-    <div className="grid place-items-center">
+    <div className="grid place-items-center p-20">
       <h1 className="flex items-center">
-        <Loadin className="animate-spin" />
-        <span className="ml-5 animate-pulse font-light">Loading</span>
+        <Loadin className="animate-bounce fill-current" />
+        <span className="ml-5 animate-pulse font-light">loading...</span>
       </h1>
     </div>
   )
