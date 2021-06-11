baea712840295ee1e432c0697ed74ad2f32a213b
Anyesh <sthacruz@gmail.com>
2021-06-11 14:40:20 +0545

    chore: User SWR for data fetching

--- components/PostDetails/index.tsx
+++ components/PostDetails/index.tsx
@@ -1,52 +1,49 @@
-import { fetchPostDetail } from '@actions/posts'
 import Loading from '@components/Common/Loading'
-import { IContent, IContentRecord } from '@types'
+import { fetcher } from '@config/axios-config'
+import isEmpty from '@validations/is-empty'
 import { Session } from 'next-auth'
 import Image from 'next/image'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+import useSWR from 'swr'
 
 function PostDetails({ id, session }: { id: string; session: Session }): React.ReactElement {
-  const [data, setData] = useState<IContent>()
-
-  useEffect(() => {
-    const fetcher = async () => {
-      const response = await fetchPostDetail(session?.accessToken as string, id)
-      if (response.error) {
-        console.table(response.error)
-      } else {
-        const data = response.data as IContentRecord
-
-        setData(data.result.data)
-      }
-    }
-    fetcher()
-  }, [id])
-
-  if (!data) return <Loading />
-
-  return (
-    <div className="grid md:grid-cols-2 sm:grid-cols-1 text-center p-6 ">
-      <div className="place-self-center">
-        <Image
-          className=" rounded-lg"
-          objectFit="cover"
-          alt={data?.title}
-          src={data?.src?.original?.url}
-          height={data.src?.original?.height}
-          width={data.src?.original?.width}
-        />
-      </div>
-      <div className="place-self-center self-center space-y-7 p-5">
-        <h1 className="font-semibold">{data?.title}</h1>
-        <hr />
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, enim? Itaque obcaecati
-          dolor, molestias dicta officia natus nostrum sequi ea commodi, cupiditate, odio veritatis
-          minima hic quaerat aperiam quibusdam labore.
-        </p>
-      </div>
-    </div>
+  const { data, error } = useSWR(
+    !isEmpty(id) ? [`/post/${id}/`, session.accessToken] : null,
+    fetcher
   )
+
+  const renderContent = () => {
+    if (!data && !error) return <Loading />
+
+    if (error) return <div className="p-5 text-nord11"> There was an error! {error.message}</div>
+
+    const result = data?.data.result.data
+    return (
+      <div className="grid md:grid-cols-2 sm:grid-cols-1 text-center p-6 ">
+        <div className="place-self-center">
+          <Image
+            className=" rounded-lg"
+            objectFit="cover"
+            alt={result?.title}
+            src={result?.src?.original?.url}
+            height={result?.src?.original?.height}
+            width={result?.src?.original?.width}
+          />
+        </div>
+        <div className="place-self-center self-center space-y-7 p-5">
+          <h1 className="font-semibold">{result?.title}</h1>
+          <hr />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, enim? Itaque obcaecati
+            dolor, molestias dicta officia natus nostrum sequi ea commodi, cupiditate, odio
+            veritatis minima hic quaerat aperiam quibusdam labore.
+          </p>
+        </div>
+      </div>
+    )
+  }
+
+  return <>{renderContent()}</>
 }
 
 export default PostDetails
