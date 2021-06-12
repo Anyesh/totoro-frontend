9618786357b6f122d9f499b909fa20b22c57123d
Anyesh <sthacruz@gmail.com>
2021-06-12 22:06:42 +0545

    chore: Fix masonry layout

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -1,3 +1,4 @@
+import { Dots } from '@assets/IconComponents'
 import { IContent } from '@types'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -5,46 +6,51 @@ import React from 'react'
 
 const Card = ({ data }: { data: IContent }): React.ReactElement => {
   return (
-    <div className="bg-white dark:bg-nord3 rounded-lg shadow-lg " key={data?.id}>
-      <div className="col-span-3 row-span-4 p-1 m-1 transition duration-500 transform hover:-translate-y-2 motion-reduce:transition-none motion-reduce:transform-none cursor-pointer">
+    <div className="first-child bg-white dark:bg-nord3 rounded-lg shadow-lg " key={data?.id}>
+      <div className="col-span-3 row-span-4 p-1 m-1 transition duration-500 hover:transform hover:translate-y-1 cursor-pointer">
         <Link href={`/?postID=${data?.id}`} as={`/posts/${data?.id}`}>
-          <Image
-            placeholder="blur"
-            className="rounded-lg"
-            objectFit="cover"
-            alt={data?.title}
-            src={data?.src?.thumbnail?.url}
-            height={data?.src?.thumbnail?.height + 90}
-            width={data?.src?.thumbnail?.width}
-          />
+          <a>
+            <Image
+              placeholder="blur"
+              className="rounded-lg"
+              // objectFit="cover"
+              alt={data?.title}
+              src={data?.src?.thumbnail?.url}
+              height={data?.src?.thumbnail?.height + 90}
+              width={data?.src?.thumbnail?.width}
+            />
+          </a>
         </Link>
       </div>
       <div className="p-2">
-        <a
-          className="flex items-center hover:text-nord10"
-          href={`/@${data.author.username}`}
-          target="__blank"
-        >
-          <img
-            loading="lazy"
-            alt={data.author.username}
-            className="block rounded-full w-7 h-7"
-            src={data.src.thumbnail.url}
-          />
-          <span className="ml-2 text-sm font-bold"> {data.author.username} </span>
-        </a>
+        <h1 className="text-base text-left truncate overflow-ellipsis">
+          <Link href={`/?postID=${data?.id}`} as={`/posts/${data?.id}`}>
+            <a className="hover:text-nord12 transition duration-500">{data?.title}</a>
+          </Link>
+        </h1>
       </div>
-
-      {/* <div className="">
-        <header className="flex items-center justify-between leading-tight p-2 md:p-4">
-          <h1 className="text-lg">
-            <a className="no-underline hover:underline text-black" href="#">
-              {data.description}
-            </a>
-          </h1>
-          <p className="text-grey-darker text-sm">9 min ago</p>
-        </header>
-      </div> */}
+      <div className="p-2 flex justify-between">
+        <Link href={`/u/@${data.author.username}`}>
+          <a
+            className="flex items-center hover:text-nord10 transition duration-500"
+            target="__blank"
+          >
+            <img
+              loading="lazy"
+              alt={data.author.username}
+              className="block object-cover rounded-full w-7 h-7"
+              src={data.src.thumbnail.url}
+            />
+            <span className="ml-2 text-sm font-bold"> {data.author.username} </span>
+          </a>
+        </Link>
+        <button
+          type="button"
+          className="focus:outline-none transform hover:-translate-y-1 transition duration-500"
+        >
+          <Dots className="fill-current" />
+        </button>
+      </div>
     </div>
   )
 }
