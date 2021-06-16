478cee5faf6504732e1a8d718c644f3b17fd1215
Anyesh <sthacruz@gmail.com>
2021-06-16 10:49:17 +0545

    fix: Justify layout

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -8,14 +8,14 @@ const Footer = dynamic(() => import('../Footer'), { ssr: false })
 export default function Layout({ children }: { children: React.ReactNode }): React.ReactElement {
   const NAME = 'Totoro'
   return (
-    <div className="overflow-x-hidden flex flex-col min-h-screen bg-nord6 dark:bg-nord1">
-      <header id="top justify-between">
+    <div className="overflow-x-hidden flex flex-col min-h-screen bg-nord6 dark:bg-nord1 justify-between">
+      <header id="top ">
         <Nav name={NAME} />
       </header>
       <main className="content">
         <div>{children}</div>
       </main>
-      <footer className="bg-white dark:bg-nord0 dark:text-white drop-shadow-sm w-full fixed bottom-0">
+      <footer className="bg-white dark:bg-nord0 dark:text-white drop-shadow-sm w-full">
         <Footer name={NAME} />
       </footer>
     </div>
