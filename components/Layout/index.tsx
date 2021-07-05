778533d6271ae401f8193cd7f8408cabfe2729c3
Anyesh <sthacruz@gmail.com>
2021-07-05 20:32:18 +0545

    chore: Add manifest and icons

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,4 +1,5 @@
 import dynamic from 'next/dynamic'
+import Head from 'next/head'
 import React from 'react'
 
 const Nav = dynamic(() => import('../Navbar'), { ssr: false })
@@ -8,16 +9,25 @@ const Footer = dynamic(() => import('../Footer'), { ssr: false })
 export default function Layout({ children }: { children: React.ReactNode }): React.ReactElement {
   const NAME = 'Totoro'
   return (
-    <div className="overflow-x-hidden flex flex-col min-h-screen bg-nord6 dark:bg-nord1 justify-between">
-      <header id="top ">
-        <Nav name={NAME} />
-      </header>
-      <main className="content">
-        <div>{children}</div>
-      </main>
-      <footer className="bg-white dark:bg-nord0 dark:text-white drop-shadow-sm w-full">
-        <Footer name={NAME} />
-      </footer>
-    </div>
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <title>Totoro | Kawaii social networking app</title>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#2E3440" />
+      </Head>
+      <div className="overflow-x-hidden flex flex-col min-h-screen bg-nord6 dark:bg-nord1 justify-between">
+        <header id="top ">
+          <Nav name={NAME} />
+        </header>
+        <main className="content">
+          <div>{children}</div>
+        </main>
+        <footer className="bg-white dark:bg-nord0 dark:text-white drop-shadow-sm w-full">
+          <Footer name={NAME} />
+        </footer>
+      </div>
+    </>
   )
 }
--- pages/error.tsx
+++ pages/error.tsx
@@ -1,5 +1,4 @@
 import { useRouter } from 'next/router'
-
 import React, { useEffect, useState } from 'react'
 
 function error(): React.ReactElement {
